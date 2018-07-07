690d6b61f894115fc1723325bc43834e2814790b
FrezDayi <40903310+FrezDayi@users.noreply.github.com>
2018-07-07 20:31:59 +0300

    Update bilgi.js

--- komutlar/bilgi.js
+++ komutlar/bilgi.js
@@ -14,7 +14,7 @@ exports.run = (client, message) => {
     .setColor(0x00AE86)
     .setTimestamp()
     .setAuthor(message.author.username, message.author.avatarURL)
-    .setDescription('Bot sürümü: 1.0.0  Yapımcı: SuperMan - Batman Bot Ekibi  :fire:  **:fire:**\n\n_**BOT Davet Linki:**_\n\nhttps://discordapp.com/api/oauth2/authorize?client_id=464805066155622401&permissions=2146958583&scope=bot \n\n_**Bot Destek Sunucusu: https://discord.gg/hxB6KNN **_\n\n**SuperMan** sunucusunun davet linki: https://discord.gg/hxB6KNN  \n\n**:copyright: 2018 SuperMan**');
+    .setDescription('Bot sürümü: 1.0.0  Yapımcı: Thor - Batman Bot Ekibi  :fire:  **:fire:**\n\n_**BOT Davet Linki:**_\n\nhttps://discordapp.com/api/oauth2/authorize?client_id=465206383290089503&permissions=2146958583&scope=bot \n\n_**Bot Destek Sunucusu: https://discord.gg/hxB6KNN **_\n\n**Thor** sunucusunun davet linki: https://discord.gg/hxB6KNN  \n\n**:copyright: 2018 Thor**');
     return message.author.sendEmbed(pingozel)
 };
 
