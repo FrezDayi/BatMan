6cb9193349ae62eae3da8613ba0b2463690b2fcb
FrezDayi <40903310+FrezDayi@users.noreply.github.com>
2018-07-07 03:44:34 +0300

    Update temizle.js

--- komutlar/temizle.js
+++ komutlar/temizle.js
@@ -8,7 +8,7 @@ exports.run = function(client, message, args) {
   if (mesajsayisi.length < 1) return message.channel.send('Kaç mesaj silmem gerektiğini belirtmedin.')
   if (mesajsayisi > 100) return message.channel.send('100 adetden fazla mesaj silemem!');
   message.channel.bulkDelete(mesajsayisi + 1);
-  message.channel.send(mesajsayisi +' adet mesaj sildim Patron**!')
+  message.channel.send(mesajsayisi +' **adet mesaj sildim Patron**!')
 };
 
 exports.conf = {
