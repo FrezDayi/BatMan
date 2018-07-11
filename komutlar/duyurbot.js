43b3e8cf399420cdb37d2a8171ebd3e304dd9571
FrezDayi <40903310+FrezDayi@users.noreply.github.com>
2018-07-11 22:57:29 +0300

    Update duyurbot.js

--- komutlar/duyurbot.js
+++ komutlar/duyurbot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
-  if (message.author.id != "326426676508622848") return message.channel.send("Bunun için iznin yok");
+  if (message.author.id != "274252490948476929") return message.channel.send("Bunun için iznin yok");
 
   let mesaj = args.slice(0).join(' ');
 if (mesaj.length < 1) return message.channel.send('Birşey Yazmalısınız');
